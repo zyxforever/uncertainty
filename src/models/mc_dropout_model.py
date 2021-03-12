b68fb53c8cfc8aacf03eaeb034f1c31fcb62063b
zyxforever <384498377@163.com>
2021-03-12 10:50:03 +0800

    nothing

--- src/models/mc_dropout_model.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn 
-
-class McDropoutModel(nn.Module):
-    def __init__(self):
-        super(McDropoutModel,self).__init__()
-        
-    def forward(self,x):
-        return x
